d47502bc3c1aea226e7871fbebd38188bc3025d5
Denis Putnam <denisputnam@verizon.net>
2020-05-19 16:58:08 -0400

    Removed sleep from stop.sh and made the calls asynchronous.

--- src/stop.sh
+++ src/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-curl  -H "Content-Type: application/json" -X POST http://localhost:3001/stop
-sleep 5
-curl  -H "Content-Type: application/json" -X POST http://localhost:3002/stop
+curl  -H "Content-Type: application/json" -X POST http://localhost:3001/stop &
+#sleep 5
+curl  -H "Content-Type: application/json" -X POST http://localhost:3002/stop &
