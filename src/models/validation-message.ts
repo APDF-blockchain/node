252723850aa7bbf2e7630a6848ca20f01ce8484c
Denis Putnam <denisputnam@verizon.net>
2020-06-04 15:54:16 -0400

    Removed some unused code and reran typedoc.

--- src/models/validation-message.ts
+++ src/models/validation-message.ts
@@ -1,5 +1,16 @@
+/**
+ * @classdesc - a model class for holding a validation message.
+ * @class ValidationMessage
+ */
 export class ValidationMessage {
+    /**
+     * @description - a string that contains the message.
+     */
     public message: string;
+
+    /**
+     * @constructor
+     */
     constructor() {
 
     }
