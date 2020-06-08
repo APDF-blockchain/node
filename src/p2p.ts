11f3ddb7f7b86848a4c58e21780f96a57738a83a
Olivier Riccini <olivier-webdev@hotmail.com>
2020-06-07 21:28:31 -0400

    ws test 18

--- src/p2p.ts
+++ src/p2p.ts
@@ -86,9 +86,9 @@ export class P2P {
      */
     public initP2PServer(p2pPort: number) {
         this.mylistenerPort = p2pPort;
-        this.mylistenerUrl = 'ws://' + this.mylistenerHost + ':' + this.mylistenerPort;
+        this.mylistenerUrl = 'ws://' + this.mylistenerHost + '/stream' + ':' + this.mylistenerPort;
         console.log('*************************************** ', this.mylistenerUrl);
-        const server: Server = new WebSocket.Server({ port: p2pPort });
+        const server: Server = new WebSocket.Server({ host: this.mylistenerHost + '/stream', port: p2pPort });
         server.on('connection', (ws: WebSocket, req: Request) => {
             //console.log('req='+JSON.stringify(req));
             let remoteAddress: string = req.url;
