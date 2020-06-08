ed6fd7fa587fa1fbb55572d191f6d73916ea7636
Olivier Riccini <olivier-webdev@hotmail.com>
2020-06-07 20:06:25 -0400

    ws test

--- src/node.ts
+++ src/node.ts
@@ -32,7 +32,7 @@ export class Node {
     /**
      * @description - p2p port number for the p2p server
      */
-    public p2pPort: number = parseInt(process.env.P2P_PORT) || this.config.defaultP2pPort;
+    public p2pPort: number = parseInt(process.env.PORT) || this.config.defaultP2pPort;
     /**
      * @description - comma separated list of peer urls.
      */
