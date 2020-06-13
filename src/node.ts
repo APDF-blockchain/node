a082ff9f5ac87edae38cf0ddee38f6e17be589b7
Olivier Riccini <olivier-webdev@hotmail.com>
2020-06-13 10:45:58 -0400

    try new deployement

--- src/node.ts
+++ src/node.ts
@@ -28,10 +28,12 @@ export class Node {
     /**
      * @description - http port for the http server
      */
+    // public httpPort: number = parseInt(process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.HTTP_PORT) || this.config.defaultServerPort;
     public httpPort: number = parseInt(process.env.HTTP_PORT) || this.config.defaultServerPort;
     /**
      * @description - p2p port number for the p2p server
      */
+    // public p2pPort: number = parseInt(process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.P2P_PORT) || this.config.defaultP2pPort;
     public p2pPort: number = parseInt(process.env.P2P_PORT) || this.config.defaultP2pPort;
     /**
      * @description - comma separated list of peer urls.
